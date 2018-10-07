0a21982dff7418c8b4c74a9975809fc45f25a767
Carson Kelley <tennisc107@gmail.com>
2018-10-06 19:51:28 -0500

    added loginPage component and test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,7 +13,11 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-export { firebase, database as default };
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+
+
+export { firebase, googleAuthProvider, database as default };
 
 // database.ref('notes/-LNKpa2t6r65gGR8xXQk').remove();
 
