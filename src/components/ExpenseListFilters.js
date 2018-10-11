6d13c029def059e5e34171e168e90e5a230219e1
Carson Kelley <tennisc107@gmail.com>
2018-10-11 07:23:02 -0500

    styling ExpenseForm and EditExpensePage

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -30,15 +30,18 @@ export class ExpenseListFilters extends React.Component {
                 <div className="input-group">
                     <div className="input-group__item">
                         <input 
-                        type="text" 
-                        value={this.props.filters.text} 
-                        onChange={this.onTextChange}
+                            type="text" 
+                            className="text-input"
+                            placeholder="Search expenses"
+                            value={this.props.filters.text} 
+                            onChange={this.onTextChange}
                         />
                     </div>
                     <div className="input-group__item">
                         <select 
-                        value={this.props.filters.sortBy} 
-                        onChange={this.onSortChange}
+                            className="select"
+                            value={this.props.filters.sortBy} 
+                            onChange={this.onSortChange}
                         >
                             <option value="date">Date</option>
                             <option value="amount">Amount</option>
